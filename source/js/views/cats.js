7fd976afe68103c8d8ad16b64137b279a65e5acb
kitajchuk <kitajchuk@gmail.com>
2018-06-25 21:14:31 -0700

    text links update

--- source/js/views/cats.js
+++ source/js/views/cats.js
@@ -1,7 +1,7 @@
 export default ( module ) => {
     let meow = module.defaultCategory;
     const html = `<div class="menu__wrap">
-        <a class="menu__a menu__a--${module.defaultCategory} js-menu-category js-menu--everything p a a--grey ${module.json.categoryFilter ? "" : "is-active"}" data-cat="${module.defaultCategory}" href="${module.json.collection.fullUrl}">${module.defaultCategory}</a>
+        <a class="menu__a menu__a--${module.defaultCategory} js-menu-category js-menu--everything p a a--lit ${module.json.categoryFilter ? "" : "is-active"} -grey" data-cat="${module.defaultCategory}" href="${module.json.collection.fullUrl}">${module.defaultCategory}</a>
         ${module.json.collection.categories.reverse().map(( category ) => {
             const cat = category.toLowerCase();
             const isCat = (module.json.categoryFilter && module.json.categoryFilter === category);
@@ -10,7 +10,7 @@ export default ( module ) => {
                 meow = cat;
             }
 
-            return `<a class="menu__a menu__a--${cat} js-menu-category js-menu--${cat} p a a--grey ${isCat ? "is-active" : ""}" data-cat="${cat}" href="${module.json.collection.fullUrl}?category=${category}">${category}</a>`;
+            return `<a class="menu__a menu__a--${cat} js-menu-category js-menu--${cat} p a a--lit ${isCat ? "is-active" : ""} -grey" data-cat="${cat}" href="${module.json.collection.fullUrl}?category=${category}">${category}</a>`;
 
         }).join( "" )}
     </div>`;
